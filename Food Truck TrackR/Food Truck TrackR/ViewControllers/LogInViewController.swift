4eda9f6f1ab455f4bc0f37c3b7f86d7b08e17611
Juan M Mariscal <j.m.mariscal03@gmail.com>
2020-08-19 19:06:38 -0700

    First Commit

---
+++ Food Truck TrackR/Food Truck TrackR/ViewControllers/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  Food Truck TrackR
+//
+//  Created by Juan M Mariscal on 8/19/20.
+//  Copyright © 2020 Juan M Mariscal. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Food Truck TrackR/Food Truck TrackR/ViewControllers/TruckDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TruckDetailViewController.swift
+//  Food Truck TrackR
+//
+//  Created by Juan M Mariscal on 8/19/20.
+//  Copyright © 2020 Juan M Mariscal. All rights reserved.
+//
+
+import UIKit
+
+class TruckDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
